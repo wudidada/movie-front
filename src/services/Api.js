cf7f003d7db8d822df021e5f92c52f06b64c8a58
Jusbin <943135242@qq.com>
2021-02-17 19:44:54 +0800

    change api host

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8888/api",
+  baseURL: "http://www.stupidbird.xyz:8888/api",
   headers: {
     "Content-type": "application/json"
   }
