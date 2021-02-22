d7629b900c1757a5397d23540a9152d4abf6fcbd
jusbin <hackerjiang@gmail.com>
2021-02-23 06:04:50 +0800

    change api host

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8888/api",
+  baseURL: "http://192.168.1.106:8888/api",
   headers: {
     "Content-type": "application/json"
   }
